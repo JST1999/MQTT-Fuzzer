148b26cd0a70348333e0033665531d63c09db1f9
Jason Tungay <jtungay@gmail.com>
2021-04-22 20:49:03 +0100

    the other part of the first fuzzing attempt

--- mqtt client subscribe.py
+++ mqtt client subscribe.py
@@ -6,14 +6,16 @@ import datetime
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
     
+
+    
 # The callback function for received message
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+msg.payload.decode("utf-8"))
-
     with open("received_log.csv", 'a', encoding="utf-8", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow([datetime.datetime.now(), msg.topic, msg.payload.decode("utf-8")])
+        csvwriter.writerow([datetime.datetime.now(), msg.topic, msg.payload.decode("utf-8", "ignore")])
     
+    #print(msg.topic+" "+msg.payload.decode("utf-8", "ignore"))
+
 client = mqtt.Client()
 client.username_pw_set(username="username",password="password")
 client.on_connect = on_connect
