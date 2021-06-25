cce943b0f5277a5610e1c763bda9f9ced4416553
Jason Tungay <jtungay@gmail.com>
2021-06-25 22:58:35 +0100

    committing some files used with the broker such as the config file and the edited makefile. Committing in case Ubuntu VM 'breaks' which it has just done

---
+++ Helpful files for MQTT broker/createLog.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /var/log/
+sudo mkdir mosquitto
+cd mosquitto
+sudo touch mosquitto.log
+sudo chmod 777 mosquitto.log
---
+++ Helpful files for MQTT broker/createPid.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /var/run/
+sudo mkdir mosquitto
+cd mosquitto
+sudo touch mosquitto.pid
+sudo chmod 777 mosquitto.pid
