40ec706c5f6a923fd6765db09de1d81f4a97bd29
Jason Tungay <jtungay@gmail.com>
2021-07-01 00:55:34 +0100

    changed name of the client file + changed some connection details. Some more testing to be done

--- Proxy fuzzer/Message fuzz/mqtt publish for proxy.py
+++ Proxy fuzzer/Message fuzz/mqtt publish for proxy.py
@@ -11,20 +11,20 @@ def on_connect(client, userdata, flags, rc):
 client = mqtt.Client()
 client.username_pw_set(username="username",password="password")
 client.on_connect = on_connect
-client.connect("192.168.0.16", 1883, 60)
+client.connect("192.168.0.25", 9998, 60)
 
 def write_to_file(topic, case):
     with open("sent_log.csv", 'a', encoding="utf-8", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow([datetime.datetime.now(), topic, case])
 
-for i in range(100000):
+for i in range(10):
     case = "Sending message number " + str(i)
 
     client.publish(topic, payload=case, qos=0, retain=False)
     write_to_file(topic, case)
 
-    time.sleep(1)
+    #time.sleep(1)
 
 print("Done")
 
