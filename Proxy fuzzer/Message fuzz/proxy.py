294dc7e45f67489e914145f607412df098d33171
Jason Tungay <47395303+JST1999@users.noreply.github.com>
2021-07-01 00:49:34 +0100

    bugs fixes + removed unnecessary functionality from the proxy i.e. receive first. Through testing, it is best to mutate line 46 of the buffer which would be each message or a combination of messages. The client connection should be ended and a new one should start due to the way TCP and the proxy work.

--- Proxy fuzzer/Message fuzz/proxy.py
+++ Proxy fuzzer/Message fuzz/proxy.py
@@ -14,7 +14,7 @@ def write_to_file(original):
     with open("publisher_log_proxy.csv", 'a', encoding="utf-8", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow([datetime.datetime.now(), original, "MESSAGE NOT FUZZED"])
-def write_to_file(original, case):
+def write_to_file_c(original, case):
     with open("publisher_log_proxy.csv", 'a', encoding="utf-8", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow([datetime.datetime.now(), original, case])
@@ -38,10 +38,26 @@ def hexdump(src, length=16, show=True):
 
 def receive_from(connection):
     buffer = b""
-    connection.settimeout(10)
+    connection.settimeout(15)
     try:
+        #buffer = connection.recv(4096)
+        counter = 1
         while True:
             data = connection.recv(4096)
+            print("---")
+            print(data)
+            print("^^^")
+
+            if counter % 2 == 0:
+                og = data
+                data = request_handler(data)
+                write_to_file_c(og, data)
+            else:
+                write_to_file(data)
+
+            counter += 1
+
+            #data = connection.recvmsg(4096)[0]#65535
             if not data:
                 break
 
@@ -55,9 +71,11 @@ def receive_from(connection):
 
 def request_handler(buffer):
     test = buffer
-    case = rad.fuzz(test.encode("UTF-8"))
-    decodedCase = case.decode("UTF-8", "ignore")
-    print(decodedCase)
+    case = rad.fuzz(test)#.encode("UTF-8"))
+    #decodedCase = case.decode("UTF-8", "ignore")
+    print("***")
+    print(case)
+    print("<|>")
 
     return buffer
 
@@ -66,21 +84,11 @@ def response_handler(buffer):
     return buffer
 
 
-def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+def proxy_handler(client_socket, remote_host, remote_port):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     remote_socket.connect((remote_host, remote_port))
 
-    if receive_first:
-        remote_buffer = receive_from(remote_socket)
-        if len(remote_buffer):
-            print("[<==] Received %d bytes from remote." % len(remote_buffer))
-            hexdump(remote_buffer)
-
-            # remote_buffer = response_handler(remote_buffer)
-            # client_socket.send(remote_buffer)
-            # print("[==>] Sent to local.")
-
-    counter = 1
+    # counter = 1
 
     while True:
         local_buffer = receive_from(client_socket)
@@ -88,17 +96,17 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             print("[<==] Received %d bytes from local." % len(local_buffer))
             hexdump(local_buffer)
 
-            if counter % 2 == 0:
-                og = local_buffer
-                local_buffer = request_handler(local_buffer)
-                write_to_file(og, local_buffer)
-            else:
-                write_to_file(local_buffer)
+            # if counter % 2 == 0:
+            #     og = local_buffer
+            #     local_buffer = request_handler(local_buffer)
+            #     write_to_file_c(og, local_buffer)
+            # else:
+            #     write_to_file(local_buffer)
             
             remote_socket.send(local_buffer)
             print("[==>] Sent to remote.")
 
-            counter += 1
+            # counter += 1
 
         remote_buffer = receive_from(remote_socket)
         if len(remote_buffer):
@@ -109,14 +117,14 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             client_socket.send(remote_buffer)
             print("[==>] Sent to local.")
 
-        if not len(local_buffer) or not len(remote_buffer):
-            client_socket.close()
-            remote_socket.close()
-            print("[*] No more data. Closing connections.")
-            break
+        # if not len(local_buffer) or not len(remote_buffer):
+        #     client_socket.close()
+        #     remote_socket.close()
+        #     print("[*] No more data. Closing connections.")
+        #     break
 
 
-def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+def server_loop(local_host, local_port, remote_host, remote_port):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         server.bind((local_host, local_port))
@@ -135,15 +143,15 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
         proxy_thread = threading.Thread(
             target=proxy_handler,
             args=(client_socket, remote_host,
-                  remote_port, receive_first))
+                  remote_port))
         proxy_thread.start()
 
 
 def main():
-    if len(sys.argv[1:]) != 5:
+    if len(sys.argv[1:]) != 4:
         print("Usage: ./proxy.py [localhost] [localport]", end='')
-        print("[remotehost] [remoteport] [receive_first]")
-        print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000 True")
+        print("[remotehost] [remoteport]")
+        print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000")
         sys.exit(0)
     
     local_host = sys.argv[1]
@@ -152,15 +160,8 @@ def main():
     remote_host = sys.argv[3]
     remote_port = int(sys.argv[4])
 
-    receive_first = sys.argv[5]
-
-    if "True" in receive_first:
-        receive_first = True
-    else:
-        receive_first = False
-
     server_loop(local_host, local_port,
-                remote_host, remote_port, receive_first)
+                remote_host, remote_port)
 
 
 if __name__ == '__main__':
