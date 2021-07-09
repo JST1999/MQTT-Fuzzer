4828547777de6df18232510b0e0298aa41151220
Jason Tungay <47395303+JST1999@users.noreply.github.com>
2021-07-09 21:45:51 +0100

    uploading logs + the changes to the proxy, now runs in vs code rather than the terminal. Keep getting fatal python error: segmentation fault

--- Proxy fuzzer/Unsubscribe fuzz/proxy.py
+++ Proxy fuzzer/Unsubscribe fuzz/proxy.py
@@ -4,10 +4,12 @@ import threading
 import csv
 import datetime
 import pyradamsa
+import faulthandler
 
 rad = pyradamsa.Radamsa()
 
-cases = [b"dev/test", b"#", b"house/+/light"]
+cases = [b"dev/test", b"#", b"house/+/light",
+b"\x104\x00\x04MQTT\x04\xc2\x00\xff\x00\x19alicedoesnotneedaclientid\x00\x05alice\x00\x06secret\x82\x19\xa5\xa6\x00\x15hello/topic/of/alice\x00"]
 
 HEX_FILTER = ''.join(
     [(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
@@ -86,30 +88,33 @@ def proxy_handler(client_socket, remote_host, remote_port):
     remote_socket.connect((remote_host, remote_port))
 
     while True:
-        local_buffer = receive_from(client_socket)
-        if len(local_buffer):
-            print("[<==] Received %d bytes from local." % len(local_buffer))
-            # hexdump(local_buffer)
+        try:
+            local_buffer = receive_from(client_socket)
+            if len(local_buffer):
+                print("[<==] Received %d bytes from local." % len(local_buffer))
+                # hexdump(local_buffer)
 
-            local_buffer = request_fuzz(local_buffer)
-            
-            remote_socket.send(local_buffer)
-            print("[==>] Sent to remote.")
+                local_buffer = request_fuzz(local_buffer)
+                
+                remote_socket.send(local_buffer)
+                print("[==>] Sent to remote.")
 
-        remote_buffer = receive_from(remote_socket)
-        if len(remote_buffer):
-            print("[<==] Received %d bytes from remote." % len(remote_buffer))
-            #hexdump(remote_buffer)
+            remote_buffer = receive_from(remote_socket)
+            if len(remote_buffer):
+                print("[<==] Received %d bytes from remote." % len(remote_buffer))
+                #hexdump(remote_buffer)
 
-            #remote_buffer = response_fuzz(remote_buffer)
-            client_socket.send(remote_buffer)
-            print("[==>] Sent to local.")
+                #remote_buffer = response_fuzz(remote_buffer)
+                client_socket.send(remote_buffer)
+                print("[==>] Sent to local.")
 
-        if not len(local_buffer) or not len(remote_buffer):
-            client_socket.close()
-            remote_socket.close()
-            print("[*] No more data. Closing connections.")
-            break
+            if not len(local_buffer) or not len(remote_buffer):
+                client_socket.close()
+                remote_socket.close()
+                print("[*] No more data. Closing connections.")
+                break
+        except:
+            continue    #always a broken pipe, just moving on
 
 
 def server_loop(local_host, local_port, remote_host, remote_port):
@@ -136,17 +141,20 @@ def server_loop(local_host, local_port, remote_host, remote_port):
 
 
 def main():
-    if len(sys.argv[1:]) != 4:
-        print("Usage: ./proxy.py [localhost] [localport]", end='')
-        print("[remotehost] [remoteport]")
-        print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000")
-        sys.exit(0)
+    # if len(sys.argv[1:]) != 4:
+    #     print("Usage: ./proxy.py [localhost] [localport]", end='')
+    #     print("[remotehost] [remoteport]")
+    #     print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000")
+    #     sys.exit(0)
     
-    local_host = sys.argv[1]
-    local_port = int(sys.argv[2])
+    local_host = "192.168.0.30"#sys.argv[1]
+    local_port = 9998#int(sys.argv[2])
 
-    remote_host = sys.argv[3]
-    remote_port = int(sys.argv[4])
+    remote_host = "192.168.0.30"#sys.argv[3]
+    remote_port = 1883#int(sys.argv[4])
+
+    faulthandler.disable()
+    faulthandler.enable(all_threads=True)
 
     server_loop(local_host, local_port,
                 remote_host, remote_port)
