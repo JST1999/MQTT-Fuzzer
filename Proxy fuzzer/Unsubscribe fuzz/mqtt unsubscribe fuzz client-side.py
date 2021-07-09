2154b4be6d63c6d4698e6e2654dafced097f5602
Jason Tungay <jtungay@gmail.com>
2021-07-09 22:10:51 +0100

    client side of the last commit + folder re-structure

--- Proxy fuzzer/Unsubscribe fuzz/mqtt unsubscribe fuzz client-side.py
+++ Proxy fuzzer/Unsubscribe fuzz/mqtt unsubscribe fuzz client-side.py
@@ -11,24 +11,25 @@ client.username_pw_set(username="username",password="password")
 client.on_connect = on_connect
 client.connect("192.168.0.30", 9998, 60)
 
-cases = ["dev/test",
+cases = [#"dev/test",
 "#",
-"house/+/light"]#,
-#"\x104\x00\x04MQTT\x04\xc2\x00\xff\x00\x19alicedoesnotneedaclientid\x00\x05alice\x00\x06secret\x82\x19\xa5\xa6\x00\x15hello/topic/of/alice\x00"]
+"house/+/light",
+"\x104\x00\x04MQTT\x04\xc2\x00\xff\x00\x19alicedoesnotneedaclientid\x00\x05alice\x00\x06secret\x82\x19\xa5\xa6\x00\x15hello/topic/of/alice\x00"]
 
 # cases = ["abcdefghi", "abcdefghij", "abcdefghijk", "abcdefghijkl", "abcdefghijklm", "abcdefghijklmn", "abcdefghijklmno", "abcdefghijklmnop", "abcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopqabcdefghijklmnopq"]
 
-def write_to_file(topic, mutatedTopic):
+def write_to_file(topic):
     with open("sent_log.csv", 'a', encoding="utf-8", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow([datetime.datetime.now(), topic, mutatedTopic])
+        csvwriter.writerow([datetime.datetime.now(), topic])
 
 for ele in cases:
     # client.subscribe(ele)
-    for i in range(2):
+    for i in range(1000):
+        write_to_file(ele)
         client.unsubscribe(ele)
         client.disconnect()
-        time.sleep(0.001)
+        time.sleep(0.1)#seperates each connection
         client.reconnect()
 
 print("Done")
