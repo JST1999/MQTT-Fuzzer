bfbc0573fcdd6e6432f02f775137f72fffd0323a
Jason Tungay <47395303+JST1999@users.noreply.github.com>
2021-07-29 01:06:08 +0100

    In HiveMQ \xc2\x81 decoded into utf-8 (its like a ^ but smaller) gives the same issue as the 201+ sub-levels issue in mosquitto. That client's publishes wont get received (by the subs) until the connection is reset

--- Simple fuzzer/topic fuzz/hive topic test.py
+++ Simple fuzzer/topic fuzz/hive topic test.py
@@ -15,17 +15,28 @@ client.username_pw_set(username="username",password="password")
 client.on_connect = on_connect
 client.connect("192.168.0.30", 1883, 60)
 
-cases = ['dev/ó ‡test',
-'"dev/te',
-'st"',
-'â€†dev/test',
-'sÂ·t',
-'dev/tesó €»t',
-'â€¯ó žtó žtesesu',
-'deó €¿dedáš€eddó Ÿâ¦áš€ó ªeó ¾ó €¾vtest',
-'dev/ó €»test',
-'eá…Ÿâ€­s'
-]
+# cases = [#'dev/ó ‡test',
+# '"dev/te',
+# 'st"',
+# 'â€†dev/test',
+# 'sÂ·t',
+# 'dev/tesó €»t',
+# #'â€¯ó žtó žtesesu',
+# #'deó €¿dedáš€eddó Ÿâ¦áš€ó ªeó ¾ó €¾vtest',
+# 'dev/ó €»test',
+# 'eá…Ÿâ€­s',
+# "normal/topic",
+# "room/light",
+# "oblivion/skyrim"
+# ]
+
+cases = ['dev/ó ‡test',
+'â€¯ó žtó žtesesu',
+'deó €¿dedáš€eddó Ÿâ¦áš€ó ªeó ¾ó €¾vtest',
+"testing/chars",
+"normal/topic",
+"room/light",
+"oblivion/skyrim"]
 
 def write_to_file(topic, case):
     with open("sent_log_topic.csv", 'a', encoding="utf-8", newline="") as csvfile:
@@ -33,7 +44,12 @@ def write_to_file(topic, case):
         csvwriter.writerow([datetime.datetime.now(), topic, case])
 
 for ele in cases:
+    time.sleep(0.1)
     print(ele)
     client.publish(ele, payload=message, qos=0, retain=False)
+    # mess = b"\xc2\x81"    #in utf-8 it is like a ^ but smaller
+    # mess2 = mess.decode("UTF-8", "ignore")
+    # print(mess2)
+    # client.publish(mess2, payload=message, qos=0, retain=False)
 
 print("Done")
